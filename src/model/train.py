9a5d315cbd54e3272d663a8e457c0d6d6ce1dc20
Louis Li <14703336+louis-li@users.noreply.github.com>
2022-07-27 13:37:24 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -22,6 +22,7 @@ def main(args):
 
     # train model
     model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    return model
 
 
 def get_csvs_df(path):
@@ -40,9 +41,9 @@ def process_data(df):
                'Age']].values, df['Diabetic'].values
 
     # train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, 
-                                                        y, 
-                                                        test_size=0.30, 
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.30,
                                                         random_state=0)
 
     # return splits and encoder
